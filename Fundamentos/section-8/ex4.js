847dd39b007a691c03b56b5374c282e80780ef87
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-09-22 15:06:11 -0300

    exercicios diarios

---
+++ Fundamentos/section-8/ex4.js
@@ -0,0 +1,36 @@
+const mage = {
+  healthPoints: 130,
+  intelligence: 45,
+  mana: 125,
+  damage: undefined,
+};
+
+const warrior = {
+  healthPoints: 200,
+  strength: 30,
+  weaponDmg: 2,
+  damage: undefined,
+};
+
+const dragon = {
+  healthPoints: 350,
+  strength: 50,
+  damage: undefined,
+};
+
+const battleMembers = { mage, warrior, dragon };
+
+const dragonAttack = (dragon) => {
+  const minDmg = 15;
+  const maxDmg = Math.floor(Math.random() * dragon.strength);
+  const dragonDmg = maxDmg > minDmg ? maxDmg : minDmg;
+  return dragonDmg;
+};
+
+const warriorAttack = (warrior) => {
+  const minDmg = warrior.strength;
+  const maxDmg = minDmg * warrior.weaponDmg;
+  const warriorDamage = Math.floor((Math.random() * (maxDmg - minDmg + 1)) + minDmg);
+  return warriorDamage;
+};
+

710e59a1187f204dea6eb44a2bdbac95fdc02cc4
Antonio Bruno Wingert <antoniobwingert@gmail.com>
2022-10-19 15:55:31 -0300

    ex diarios

---
+++ Front-End/sessao 2/day 1/exAulaAoVivo/my-app/src/About.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+class About extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Antonio Bruno Wingert</h1>
+        <p>Apenas o mais brabo</p>
+        <h2>Minhas Habilidades:</h2>
+        <ul>
+          <li>Fon</li>
+          <li>O mais brabo</li>
+          <li>Meu deus do ceu</li>
+        </ul>
+      </div>
+    )
+  }
+}
+
+export default About;
